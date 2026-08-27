ed00159cba94a840d298c79c9c8ca6069c0ea8a4
CodeWithRJ <13061978+code-with-rj1399@users.noreply.github.com>
2026-08-27 11:42:18 +0530

    Add application bootstrap

---
+++ src/main/java/com/rj1399/customersupport/CustomerSupportApplication.java
@@ -0,0 +1,11 @@
+package com.rj1399.customersupport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerSupportApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerSupportApplication.class, args);
+    }
+}
